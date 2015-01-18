9aec176496a9bdc333b0d59193172eef509e0504
Mike <mtovino@Mikes-MacBook-Air.local>
2015-01-18 12:38:46 -0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Mike on 1/18/15.
+//  Copyright (c) 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
